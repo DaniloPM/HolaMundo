0af09562bf678752708e0c9bdc989f756180a88b
Danilo Pacheco <dpm1503@gmail.com>
2017-03-21 20:40:49 -0500

    Interfaz 2.0 terminada2

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +17,14 @@ public class Principal extends AppCompatActivity {
     private EditText cajaApellido;
     private Intent i; //Pasar de una actividad a otra.
     private Bundle b; //Tener los datos encapsulados.
+    private Spinner comboGenero;
+    private RadioButton r1, r2, r3;
+    private ArrayAdapter<String> adapter;
+    private String[] opc;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +34,64 @@ public class Principal extends AppCompatActivity {
         //Capturamos las cajas.
         cajaNombre = (EditText) findViewById(R.id.txtNombre);
         cajaApellido = (EditText) findViewById(R.id.txtApellido);
+
+        //capturar combo
+        comboGenero = (Spinner)findViewById(R.id.cmdGenero);
+
+
+
+        //capturar Radios
+        r1= (RadioButton)findViewById(R.id.r1);
+        r2= (RadioButton)findViewById(R.id.r2);
+        r3= (RadioButton)findViewById(R.id.r3);
+
+
+
+
         //Creamos el objeto Intent que sirve para pasar de una actividad a otra.
         i = new Intent(this, Saludo.class);
         //Creamos el objeto Bundle que sirve para encapsular datos.
         b = new Bundle();
+
+
+        opc = this.getResources().getStringArray(R.array.generos);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,opc);
+
+        comboGenero.setAdapter(adapter);
+
+
     }
 
     public void saludar(View v) { //Hacer boton.
-        String nomb, apell;
+        String nomb, apell,genero,estado_civil=" ";
         if (validar()) {
             //Tomo el valor que la persona ingresó tanto en nombre como en apellido.
             nomb = cajaNombre.getText().toString();//Traer texto.
             apell = cajaApellido.getText().toString();//Traer texto.
+
+            //Capturar Genero
+            genero = comboGenero.getSelectedItem().toString();
+
+            //Capturar Estado Civil
+
+            if(r1.isChecked())estado_civil = getResources().getString(R.string.Soltero);
+            if(r2.isChecked())estado_civil = getResources().getString(R.string.Casado);
+            if(r3.isChecked())estado_civil = getResources().getString(R.string.Divorciado);
+
+
+
+
             //Encapsulo los valores previamente tomados.
             b.putString("Nombre", nomb);//Meter en la capsula.
             b.putString("Apellido", apell);//Meter en la capsula.
+            b.putString("Genero",genero);
+            b.putString("Estado_civil",estado_civil);
             //Le paso al Intent todos los datos en forma encapsulada con el Bundle.
             i.putExtras(b);//Pasar la capsula.
+
             //Arranco la actividad que el Intent me diga.
             startActivity(i);//Pasa a la actividad que dijo el Intent.
+
         }}
 
     public boolean validar() {
--- app/src/main/java/com/example/android/holamundo/Saludo.java
+++ app/src/main/java/com/example/android/holamundo/Saludo.java
@@ -9,7 +9,7 @@ public class Saludo extends AppCompatActivity {
 
     private TextView saludo;
     private Bundle b;
-    private String aux, nomb, apell;
+    private String aux, nomb, apell,genero,Estado_civil;
     private Resources res;
 
     @Override
@@ -21,9 +21,20 @@ public class Saludo extends AppCompatActivity {
         b = getIntent().getExtras();
         nomb = b.getString("Nombre");
         apell = b.getString("Apellido");
+        genero = b.getString("Genero");
+        Estado_civil = b.getString("Estado_civil");
         res = this.getResources();
 
-        aux = res.getString(R.string.parte_saludo)+" "+nomb+" "+apell+" "+res.getString(R.string.parte_saludo2);
+        //aux = res.getString(R.string.parte_saludo)+" "+nomb+" "+apell+" "+res.getString(R.string.parte_saludo2);
+        aux = aux+"\n"+getResources().getString(R.string.Datos);
+        aux = aux+"\n\n"+getResources().getString(R.string.Nombre)+": "+nomb;
+        aux = aux+"\n\n"+getResources().getString(R.string.Apellido)+": "+apell;
+        aux = aux+"\n\n"+getResources().getString(R.string.Genero)+": "+genero;
+        aux = aux+"\n\n"+getResources().getString(R.string.Estado_Civil)+": "+Estado_civil;
+
+
+
+
         saludo.setText(aux);
     }
 }
